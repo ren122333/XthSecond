2569d48fb7ddc1667e30cfc97f25c53fa83c4414
ren122333 <danyabogdanovski@gmail.com>
2026-03-09 16:09:25 +0200

    first commit

---
+++ Main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+
+    int sec;
+    int hour;
+    int minute;
+    scanf("%d",&sec);
+    if (sec > 86400 || sec < 0){
+        return -1;
+    }
+    hour = sec / 3600;
+    minute = sec % 3600 / 60;
+    printf("%d hours, %d minutes.", hour, minute);
+    return 0;
+}
